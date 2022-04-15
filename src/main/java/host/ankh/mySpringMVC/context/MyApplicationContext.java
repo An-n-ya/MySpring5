8829791e89ab46fe8c81f742c2e592bc175e76b3
ankh04 <ankh04@icloud.com>
2022-04-15 11:20:34 +0800

    mySpring DI框架 done

--- src/main/java/host/ankh/mySpringMVC/context/MyApplicationContext.java
+++ src/main/java/host/ankh/mySpringMVC/context/MyApplicationContext.java
@@ -1,9 +1,13 @@
 package host.ankh.mySpringMVC.context;
 
+import host.ankh.mySpringMVC.annotation.MyAutowired;
+import host.ankh.mySpringMVC.annotation.MyController;
+import host.ankh.mySpringMVC.annotation.MyService;
 import host.ankh.mySpringMVC.core.MyBeanDefinition;
 import host.ankh.mySpringMVC.core.MyBeanFactory;
 import host.ankh.mySpringMVC.core.MyBeanWrapper;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -88,7 +92,32 @@ public class MyApplicationContext extends MyDefaultListableBeanFactory implement
     @Override
     public Object getBean(String beanName) throws Exception{
         // 在这里进行 DI AOP 等操作
-        return null;
+        // 从父类中拿到 beanDefinition
+        MyBeanDefinition beanDefinition = super.beanDefinitionMap.get(beanName);
+
+        // 初始化后处理器
+        MyBeanPostProcessor beanPostProcessor = new MyBeanPostProcessor();
+
+        // 将beanDefinition转化为实例
+        Object instance = initiateBean(beanDefinition);
+        // 确保实例化成功, 才进行后面的操作
+        if (instance == null) return null;
+
+        // 后处理器调用
+        instance = beanPostProcessor.postProcessBeforeInitialization(instance, beanName);
+
+        // 引入 wrapper 是为了方便在 wrapper 中对bean进行操作
+        MyBeanWrapper beanWrapper = new MyBeanWrapper(instance);
+        // 将 wrapper 放入 IOC 容器中
+        this.factoryBeanInstanceCache.put(beanName, beanWrapper);
+
+        // 处理autowired
+        populateBean(beanName, instance);
+
+        // 初始化完毕, 调用后处理器
+        instance = beanPostProcessor.postProcessAfterInitialization(instance, beanName);
+
+        return this.factoryBeanInstanceCache.get(beanName).getWrapperInstance();
     }
 
     @Override
@@ -108,4 +137,58 @@ public class MyApplicationContext extends MyDefaultListableBeanFactory implement
     public Properties getConfig() {
         return this.reader.getConfig();
     }
+
+    // 将 beanDefinition 转化为实例
+    public Object initiateBean(MyBeanDefinition beanDefinition) {
+        Object instance = null;
+        String className = beanDefinition.getBeanClassName();
+
+        // 先查一下IOC容器中是否已经有了
+        if (this.factoryBeanObjectCache.containsKey(className)) {
+            instance = this.factoryBeanObjectCache.get(className);
+        } else {
+            // 如果容器没有, 利用反射自己造
+            try {
+                Class<?> clazz = Class.forName(className);
+                instance = clazz.newInstance();
+                this.factoryBeanObjectCache.put(beanDefinition.getBeanClassName(), instance);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
+    }
+
+    // 对该对象里的 autowired 属性进行注入
+    public void populateBean(String beanName, Object instance) {
+        Class<?> clazz = instance.getClass();
+
+        if (!(clazz.isAnnotationPresent(MyController.class) || clazz.isAnnotationPresent(MyService.class))) {
+            // 如果类没有被 @Controller 和 @MyService 注解, 直接返回
+            return;
+        }
+
+        // 获取所有的域, 并检查每个域是否被 autowired 注解
+        Field[] fields = clazz.getDeclaredFields();
+        for(Field field : fields) {
+            if (!field.isAnnotationPresent(MyAutowired.class)) continue;
+
+            MyAutowired autowired = field.getAnnotation(MyAutowired.class);
+            String autowiredBeanName = autowired.value().trim();
+
+            // 如果注解的名称为空, 使用类型注解
+            if ("".equals(autowiredBeanName)) {
+                autowiredBeanName = field.getType().getName();
+            }
+
+            // 进行注入
+            field.setAccessible(true);
+            try {
+                field.set(instance, this.factoryBeanInstanceCache.get(autowiredBeanName).getWrapperInstance());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
