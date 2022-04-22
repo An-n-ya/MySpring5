380bbbc19874f723cd2fa714035d748c76783f29
ankh04 <ankh04@icloud.com>
2022-04-22 19:38:19 +0800

    MVC 初步

--- src/main/java/host/ankh/mySpringMVC/v1/servlet/MyDispatcherServlet.java
+++ src/main/java/host/ankh/mySpringMVC/v1/servlet/MyDispatcherServlet.java
@@ -2,7 +2,7 @@ package host.ankh.mySpringMVC.v1.servlet;
 
 import host.ankh.mySpringMVC.annotation.MyController;
 import host.ankh.mySpringMVC.annotation.MyRequestMapping;
-import host.ankh.mySpringMVC.annotation.MyService;
+import host.ankh.mySpringMVC.context.MyApplicationContext;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -11,99 +11,177 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 
 /**
  * @author ankh
  * @created at 2022-03-09 11:30 AM
  */
 public class MyDispatcherServlet extends HttpServlet {
+    private final String LOCATION = "contextConfigLocation";
     private Map<String, Object> mapping = new HashMap<>();
+
+    private MyApplicationContext context;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        InputStream is = null;
+        context = new MyApplicationContext(config.getInitParameter(LOCATION));
+        initStrategies(context);
+    }
+
+    protected void initStrategies(MyApplicationContext context) {
+        // 一共有九种策略
+        // 对于每个用户请求, 这九种策略都会进行处理
+
+        // ============= 在这里处理九种策略 ===============
+        initMultipartResolver(context); // 文件上传解析
+        initLocaleResolver(context); // 本地化解析
+        initThemeResolver(context); // 主体解析
+        initHandlerMappings(context); // 通过 handlerMapping 映射请求到处理 (自己实现)
+        initHandlerAdapters(context); // 通过 handlerAdapters 进行多类型的参数动态匹配 (自己实现)
+        initHandlerExceptionResolvers(context); // 如果执行出现异常, 交给 HandlerExceptionResolver 解析
+        initRequestToViewNameTranslator(context); // 将请求映射到视图
+        initViewResolvers(context); // 通过 viewResolver 将逻辑视图解析到具体视图(自己实现)
+        initFlashManager(context); // Flash映射管理器
+    }
+
+    private void initFlashManager(MyApplicationContext context) {}
+    private void initRequestToViewNameTranslator(MyApplicationContext context) {}
+    private void initHandlerExceptionResolvers(MyApplicationContext context) {}
+    private void initThemeResolver(MyApplicationContext context) {}
+    private void initLocaleResolver(MyApplicationContext context) {}
+    private void initMultipartResolver(MyApplicationContext context) {}
+
+    private void initHandlerMappings(MyApplicationContext context) {
+        // 先从容器中取出所有的实例
+        String[] beanNames = context.getBeanDefinitionNames();
         try {
-            Properties configContext = new Properties();
-            is = this.getClass().getClassLoader().getResourceAsStream(config.getInitParameter("contextConfigLocation"));
-            configContext.load(is);
-            String scanPackage = configContext.getProperty("scanPackage");
-            doScanner(scanPackage);
-            // 这里如果直接使用mapping.keySet()做遍历会出错,所以就用了这种麻烦的形式
-            Set<String> classNames = mapping.keySet();
-            Object[] classArr = classNames.toArray();
-            for (Object classNameObj : classArr) {
-                String className = (String) classNameObj;
-                if (!className.contains(".")) continue;
-                // 利用反射获取Class
-                Class<?> clazz = Class.forName(className);
-                if (clazz.isAnnotationPresent(MyController.class)) {
-                    // 如果是controller注解的类
-                    mapping.put(className, clazz.newInstance());
-
-                    // 处理在类上注解的MyRequestMapping,这个url作为基准url
-                    String baseUrl = "";
-                    if (clazz.isAnnotationPresent(MyRequestMapping.class)) {
-                        // 获取url信息
-                        MyRequestMapping requestMapping = clazz.getAnnotation(MyRequestMapping.class);
-                        baseUrl = requestMapping.value();
-                    }
-                    Method[] methods = clazz.getMethods();
-                    for (Method method : methods) {
-                        // 遍历controller类里的每一个被MyRequestMapping注解的方法
-                        if (!method.isAnnotationPresent(MyRequestMapping.class)) continue;
-
-                        // 获取url信息
-                        MyRequestMapping requestMapping = method.getAnnotation(MyRequestMapping.class);
-                        String subUrl = requestMapping.value();
-                        String url = baseUrl + subUrl;
-                        // 将方法放入"IOC容器"
-                        mapping.put(url, method);
-                        System.out.println("Mapped " + url + "," + method);
-                    }
-
-                } else if (clazz.isAnnotationPresent(MyService.class)) {
-                    // 如果是service注解的类
-                    MyService service = clazz.getAnnotation(MyService.class);
-                    // 获取service注解定义的bean名称
-                    String beanName = service.value();
-                    // 如果没有设置bean名称,则设置为类名
-                    if("".equals(beanName)) beanName = clazz.getName();
-                    // 创建实例
-                    Object obj = clazz.newInstance();
-                    // 将创建出来的实例放入"IOC容器"
-                    mapping.put(beanName, obj);
-
-                    // service 所实现的接口也都需要有对应的bean
-                    for (Class<?> i: clazz.getInterfaces()) {
-                        mapping.put(i.getName(), obj);
-                    }
-
-                } else {
+            for (String beanName : beanNames) {
+                Object controller = context.getBean(beanName);
+                Class<?> clazz = controller.getClass();
+                if (!clazz.isAnnotationPresent(MyController.class)) {
+                    // 如果没有被 controller 注解, 直接跳过
                     continue;
                 }
-            }
-
-
-        } catch (Exception e) {} finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                // 处理在类上注解的MyRequestMapping,这个url作为基准url
+                String baseUrl = "";
+                if (clazz.isAnnotationPresent(MyRequestMapping.class)) {
+                    // 获取url信息
+                    MyRequestMapping requestMapping = clazz.getAnnotation(MyRequestMapping.class);
+                    baseUrl = requestMapping.value();
                 }
+                Method[] methods = clazz.getMethods();
+                for (Method method : methods) {
+                    // 遍历controller类里的每一个被MyRequestMapping注解的方法
+                    if (!method.isAnnotationPresent(MyRequestMapping.class)) continue;
+
+                    // 获取url信息
+                    MyRequestMapping requestMapping = method.getAnnotation(MyRequestMapping.class);
+                    String subUrl = requestMapping.value();
+                    String url = baseUrl + subUrl;
+                    // 将方法放入"IOC容器"
+                    mapping.put(url, method);
+                    System.out.println("Mapped " + url + "," + method);
+                }
+
+
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        super.init(config);
     }
+
+    /**
+     * 将 Request 的字符型参数自动适配为 Method 的 Java 实参, 主要实现参数列表自动适配和类型转换功能
+     * @param context
+     */
+    private void initHandlerAdapters(MyApplicationContext context) {
+
+    }
+
+    private void initViewResolvers(MyApplicationContext context) {
+
+    }
+//    public void init(ServletConfig config) throws ServletException {
+//        InputStream is = null;
+//        try {
+//            Properties configContext = new Properties();
+//            is = this.getClass().getClassLoader().getResourceAsStream(config.getInitParameter("contextConfigLocation"));
+//            configContext.load(is);
+//            String scanPackage = configContext.getProperty("scanPackage");
+//            doScanner(scanPackage);
+//            // 这里如果直接使用mapping.keySet()做遍历会出错,所以就用了这种麻烦的形式
+//            Set<String> classNames = mapping.keySet();
+//            Object[] classArr = classNames.toArray();
+//            for (Object classNameObj : classArr) {
+//                String className = (String) classNameObj;
+//                if (!className.contains(".")) continue;
+//                // 利用反射获取Class
+//                Class<?> clazz = Class.forName(className);
+//                if (clazz.isAnnotationPresent(MyController.class)) {
+//                    // 如果是controller注解的类
+//                    mapping.put(className, clazz.newInstance());
+//
+//                    // 处理在类上注解的MyRequestMapping,这个url作为基准url
+//                    String baseUrl = "";
+//                    if (clazz.isAnnotationPresent(MyRequestMapping.class)) {
+//                        // 获取url信息
+//                        MyRequestMapping requestMapping = clazz.getAnnotation(MyRequestMapping.class);
+//                        baseUrl = requestMapping.value();
+//                    }
+//                    Method[] methods = clazz.getMethods();
+//                    for (Method method : methods) {
+//                        // 遍历controller类里的每一个被MyRequestMapping注解的方法
+//                        if (!method.isAnnotationPresent(MyRequestMapping.class)) continue;
+//
+//                        // 获取url信息
+//                        MyRequestMapping requestMapping = method.getAnnotation(MyRequestMapping.class);
+//                        String subUrl = requestMapping.value();
+//                        String url = baseUrl + subUrl;
+//                        // 将方法放入"IOC容器"
+//                        mapping.put(url, method);
+//                        System.out.println("Mapped " + url + "," + method);
+//                    }
+//
+//                } else if (clazz.isAnnotationPresent(MyService.class)) {
+//                    // 如果是service注解的类
+//                    MyService service = clazz.getAnnotation(MyService.class);
+//                    // 获取service注解定义的bean名称
+//                    String beanName = service.value();
+//                    // 如果没有设置bean名称,则设置为类名
+//                    if("".equals(beanName)) beanName = clazz.getName();
+//                    // 创建实例
+//                    Object obj = clazz.newInstance();
+//                    // 将创建出来的实例放入"IOC容器"
+//                    mapping.put(beanName, obj);
+//
+//                    // service 所实现的接口也都需要有对应的bean
+//                    for (Class<?> i: clazz.getInterfaces()) {
+//                        mapping.put(i.getName(), obj);
+//                    }
+//
+//                } else {
+//                    continue;
+//                }
+//            }
+//
+//
+//        } catch (Exception e) {} finally {
+//            if (is != null) {
+//                try {
+//                    is.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        super.init(config);
+//    }
 
     // 根据类地址扫描Bean,加载到IOC容器中
     private void doScanner(String scanPackage) {
@@ -172,9 +250,8 @@ public class MyDispatcherServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             doDispatch(req, resp);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
+        } catch (Exception e) {
+            resp.getWriter().write("failed");
             e.printStackTrace();
         }
     }
